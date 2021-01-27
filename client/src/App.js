ea5c2c58d53e9704c145e94ce0217155262fadb4
Wrubleski <henrike.hip@live.com>
2021-01-27 16:55:33 -0300

    Mostra, de forma bem simples, a temperatura da cidade enviada pelo input.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherReport, setWeatherReport] = useState({});
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios({
+      method: "POST",
+      url: "https://desafio-aevo-backend.herokuapp.com/current",
+      data: { city },
+    });
+
+    const data = await response.data;
+    setWeatherReport(data);
+
+    console.log(data);
+  };
+  // Evita que o react tente acessar .current.temperature de undefined no primeiro render.
+  const temperature =
+    weatherReport.current && weatherReport.current.temperature;
   return (
     <>
       <h1>Teste</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="city">Choose your city</label>
+        <input
+          type="text"
+          name="city"
+          value={city}
+          onChange={(e) => {
+            setCity(e.target.value);
+          }}
+        />
+        <button>Submit</button>
+      </form>
+      <h1>Temperatura: {temperature || null}</h1>
     </>
   );
 }
