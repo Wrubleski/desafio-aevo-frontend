44b42d4bcd3b51ddf719dd344fd958965a391931
Wrubleski <henrike.hip@live.com>
2021-02-01 14:22:19 -0300

    move up Citychart from CurrentTemperature

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import CurrentTemperature from "./components/CurrentTemperature";
+import CityChart from "./components/CityChart";
 import "./assets/css/main.css";
-import axios from "axios";
-
-axios.defaults.baseURL = "https://desafio-aevo-backend.herokuapp.com/api";
 
 function App() {
   return (
     <>
       <CurrentTemperature />
+      <CityChart />
     </>
   );
 }
